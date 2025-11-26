8587086e9e616e84dc84be688d9e9fe202e78b34
Hamza Naveed <l230618@lhr.nu.edu.pk>
2025-11-26 11:06:10 +0500

    address corrected

--- interfaces/IAddressRepository.js
+++ interfaces/IAddressRepository.js
@@ -1,23 +1,7 @@
 // src/interfaces/IAddressRepository.js
 class IAddressRepository {
-    async getAddresses(userId) {
-        throw new Error("Method 'getAddresses' must be implemented");
-    }
-
-    async createAddress({ user_id, city, country, detail }) {
-        throw new Error("Method 'createAddress' must be implemented");
-    }
-
-    async updateAddress({ addressId, city, country, detail }) {
-        throw new Error("Method 'updateAddress' must be implemented");
-    }
-
-    async deleteAddress(addressId) {
-        throw new Error("Method 'deleteAddress' must be implemented");
-    }
-
-    async deactivateAddress(addressId) {
-        throw new Error("Method 'deactivateAddress' must be implemented");
+    async execute() {
+        throw new Error("pure virtual function must be implemented");
     }
 }
 
