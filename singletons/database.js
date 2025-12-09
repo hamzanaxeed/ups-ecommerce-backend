03cb6eaab0a525e198b859ad7239bb91c432abaf
Hamza Naveed <l230618@lhr.nu.edu.pk>
2025-12-09 16:10:28 +0500

    removed fazool comment

--- singletons/database.js
+++ singletons/database.js
@@ -1,11 +1,6 @@
 const supabase = require("../db/supabaseClient");
 const dbClient = require("../core/dbClient");
 
-/**
- * Aggregator module that exposes DB singletons in one place.
- * Use `const { supabase, dbClient } = require('./singletons/database')`
- * or the getters `getSupabase()` / `getDbClient()`.
- */
 module.exports = {
     supabase,
     dbClient,
